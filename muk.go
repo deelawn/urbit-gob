921aa3d7b2f6a0fb5946fa1c3eacbd744952157e
Dylan Boltz <dboltz03@gmail.com>
2021-03-13 23:57:43 +0000

    Fixed muk bugs around runes and unsigned integer 32 types

--- muk.go
+++ muk.go
@@ -10,36 +10,36 @@ var (
 	u256   = big.NewInt(256)
 )
 
-func muk(seed uint, key *big.Int) *big.Int {
+func muk(seed uint32, key *big.Int) *big.Int {
 
 	lo := big.NewInt(0).And(key, uxFF).Int64()
 	hi := big.NewInt(0).Div(big.NewInt(0).And(key, uxFF00), u256).Int64()
-	hashKey := string(rune(lo) + rune(hi))
+	hashKey := []rune{rune(lo), rune(hi)}
 
 	hash := murmurHash(hashKey, seed)
 	return big.NewInt(int64(hash))
 }
 
-func murmurHash(key string, seed uint) uint {
+func murmurHash(key []rune, seed uint32) uint32 {
 
 	keyLen := len(key)
 	remainder := keyLen & 3 // len(key) % 4
 	bytes := keyLen - remainder
-	h1 := seed
-	c1 := uint(0xcc9e2d51)
-	c2 := uint(0x1b873593)
+	h1 := uint32(seed)
+	c1 := uint32(0xcc9e2d51)
+	c2 := uint32(0x1b873593)
 	var (
-		i   int
-		k1  uint
-		h1b uint
+		i   uint32
+		k1  uint32
+		h1b uint32
 	)
 
-	for i < bytes {
+	for int(i) < bytes {
 
-		k1 = uint((key[i] & 0xff) |
-			((key[i+1] & 0xff) << 8) |
-			((key[i+2] & 0xff) << 16) |
-			((key[i+3] & 0xff) << 24))
+		k1 = (uint32(key[i]) & 0xff) |
+			((uint32(key[i+1]) & 0xff) << 8) |
+			((uint32(key[i+2]) & 0xff) << 16) |
+			((uint32(key[i+3]) & 0xff) << 24)
 
 		i += 4
 
@@ -58,13 +58,15 @@ func murmurHash(key string, seed uint) uint {
 	switch remainder {
 
 	case 3:
-		k1 ^= uint((key[i+2] & 0xff) << 16)
+		k1 ^= uint32(key[i+2]&0xff) << 16
+		fallthrough
 
 	case 2:
-		k1 ^= uint((key[i+1] & 0xff) << 8)
+		k1 ^= uint32(key[i+1]&0xff) << 8
+		fallthrough
 
 	case 1:
-		k1 ^= uint(key[i] & 0xff)
+		k1 ^= uint32(key[i] & 0xff)
 	}
 
 	k1 = (((k1 & 0xffff) * c1) + ((((k1 >> 16) * c1) & 0xffff) << 16)) & 0xffffffff
@@ -72,7 +74,7 @@ func murmurHash(key string, seed uint) uint {
 	k1 = (((k1 & 0xffff) * c2) + ((((k1 >> 16) * c2) & 0xffff) << 16)) & 0xffffffff
 	h1 ^= k1
 
-	h1 ^= uint(len(key))
+	h1 ^= uint32(keyLen)
 
 	h1 ^= h1 >> 16
 	h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff
@@ -80,5 +82,5 @@ func murmurHash(key string, seed uint) uint {
 	h1 = (((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >> 16) * 0xc2b2ae35) & 0xffff) << 16)) & 0xffffffff
 	h1 ^= h1 >> 16
 
-	return h1 >> 0
+	return h1
 }
